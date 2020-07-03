e6912ed11b4bc0ea51e97d5c5fad2cc699e01521
erassmirza <erass.mirza@gmail.com>
2020-07-03 22:32:40 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+// =========================> Chapter 21-25 <=========================
+
+
+// 1. Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable titled fullName.
+//    Greet the user using his full name.
